48925206a9e031f0310598f499cf2ad9ec4f4e9d
Khubaib Shahid <khubaibkingofk@gmail.com>
2023-11-26 22:25:30 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+function greeting() {
+    var fname = document.getElementById("fname").value;
+    var nationality = document.getElementById("nationality").value;
+    alert("Hello " + fname + " We know you are " + nationality);
+}
